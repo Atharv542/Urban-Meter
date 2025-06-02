4b588801d3ac7d966c256ea9523d0a688b603acb
Atharv542 <162612781+Atharv542@users.noreply.github.com>
2025-06-03 01:40:57 +0530

    Update Register.jsx

--- myapp/src/pages/Register.jsx
+++ myapp/src/pages/Register.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { toast } from 'react-hot-toast';
 import axios from 'axios';
 import { useNavigate } from "react-router-dom";
-import { Link } from 'react-router-dom';
+
 
 const Register = () => {
   const [formData, setFormData] = useState({
